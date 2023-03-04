603bbaae884b90d752960a54960fa0ba97ee0b63
Brannua <374453156@qq.com>
2023-03-04 18:13:29 +0800

    do nothing

--- index.py
+++ index.py
@@ -1,7 +1,8 @@
 """
 Pseudo code:
 
-    current_link = random_link_of_a_wikipedia_page
+    current_url = # random_url_of_a_wikipedia_page
+    target_url = "https://en.wikipedia.org/wiki/Philosophy"
     article_chain = []
 
     页面标题不是哲学，并且我们尚未发现循环：
@@ -15,7 +16,7 @@ Pseudo code:
         暂停片刻
 """
 
-# current_url = # random_url_of_a_wikipedia_page
+current_url = # random_url_of_a_wikipedia_page
 target_url = "https://en.wikipedia.org/wiki/Philosophy"
 article_chain = []
 
